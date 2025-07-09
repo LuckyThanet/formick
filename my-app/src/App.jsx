2cce2583b6bd5bab6f009eda288bf9ff29ca8654
LuckyThanet <thanet.jomp@mail.kmutt.ac.th>
2025-07-10 02:03:40 +0700

    first commit

---
+++ my-app/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='bg-red p-4'>
+        hello world
+      </div>
+       
+    </>
+  )
+}
+
+export default App
